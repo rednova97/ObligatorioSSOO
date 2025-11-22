d6de4aff4ee02d9eb197d8c3f01bc1b5c76afaec
RedNova97 <108893072+RedNova97@users.noreply.github.com>
2025-11-22 13:16:04 -0300

    actualizado el main para linux kubuntu

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
 const int MOZOS = 3;
 const int COCINEROS = 3;
@@ -14,36 +10,43 @@ const int REPOSTEROS = 3;
 int main() {
 
     int p;
-    
+
+    // Lanzar reposteros
     for (int i = 0; i < REPOSTEROS; i++) {
         p = fork();
         if (p == 0) {
-            char *args[] = {"repostero.exe", NULL};
-            execv("C:\\Users\\awsba\\OneDrive\\Desktop\\OblSO\\repostero.exe", args);
+            char *args[] = {"./repostero", NULL};
+            execv("./repostero", args);
+            perror("Error al ejecutar repostero");
+            exit(1);
         }
     }
 
+    // Lanzar cocineros
     for (int i = 0; i < COCINEROS; i++) {
         p = fork();
         if (p == 0) {
-            char *args[] = {"cocinero.exe", NULL};
-            execv("C:\\Users\\awsba\\OneDrive\\Desktop\\OblSO\\cocinero.exe", args);
-
+            char *args[] = {"./cocinero", NULL};
+            execv("./cocinero", args);
+            perror("Error al ejecutar cocinero");
+            exit(1);
         }
     }
 
+    // Lanzar mozos
     for (int i = 0; i < MOZOS; i++) {
         p = fork();
         if (p == 0) {
-            char *args[] = {"mozo.exe", NULL};
-            execv("C:\\Users\\awsba\\OneDrive\\Desktop\\OblSO\\mozo.exe", args);
-
+            char *args[] = {"./mozo", NULL};
+            execv("./mozo", args);
+            perror("Error al ejecutar mozo");
+            exit(1);
         }
     }
 
+    // Esperar a todos
     for (int i = 0; i < (MOZOS + COCINEROS + REPOSTEROS); i++)
         wait(NULL);
 
     return 0;
 }
- 
